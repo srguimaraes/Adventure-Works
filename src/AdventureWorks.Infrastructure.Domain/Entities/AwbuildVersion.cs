a1c65d57fa6531909d060736e7adbd93177c2d9c
Rogério Guimarães <rogerio.guimaraes@outlook.com>
2017-07-24 13:20:35 -0400

    initial commit

---
+++ src/AdventureWorks.Infrastructure.Domain/Entities/AwbuildVersion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventureWorks.Infrastructure.Data.EF
+{
+    public partial class AwbuildVersion
+    {
+        public byte SystemInformationId { get; set; }
+        public string DatabaseVersion { get; set; }
+        public DateTime VersionDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
